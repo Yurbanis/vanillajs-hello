fdb65b4259d2d703b48666d7f9825c2616c37da4
Yurbanis <yurbanisbriceno@gmail.com>
2023-02-20 17:01:25 +0000

    proyecto dia 6

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  var pronoun = ["the", "our"];
+  var adj = ["great", "big"];
+  var noun = ["jogger", "racoon"];
+
+  function anidados(pronoun, adj, noun) {
+    let resul = "";
+
+    let number = Math.floor(Math.random() * 2);
+    resul = pronoun[number];
+
+    let number1 = Math.floor(Math.random() * 2);
+    resul += adj[number1];
+
+    let number3 = Math.floor(Math.random() * 2);
+    resul += noun[number3];
+
+    return `${resul}.com`;
+  }
+  console.log(anidados(pronoun, adj, noun));
 };
